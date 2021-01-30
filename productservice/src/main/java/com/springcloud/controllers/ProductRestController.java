8f8251680ebb9bdd898d7914f1010aebfede3d37
Natasha Juneja <natashajuneja@gmail.com>
2021-01-29 22:47:47 -0600

    Configserver changes

--- productservice/src/main/java/com/springcloud/controllers/ProductRestController.java
+++ productservice/src/main/java/com/springcloud/controllers/ProductRestController.java
@@ -4,6 +4,8 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.springcloud.model.Product;
 import com.springcloud.repos.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +17,7 @@ import com.springcloud.restclients.CouponClient;
 
 @RestController
 @RequestMapping("/productapi")
+@RefreshScope
 public class ProductRestController {
 
 	@Autowired
@@ -23,13 +26,15 @@ public class ProductRestController {
 	@Autowired
 	private ProductRepo repo;
 
+	@Value("${com.test.springcloud.prop}")
+	private String prop;
+
 	@HystrixCommand(fallbackMethod = "sendErrorResponse")
 	@RequestMapping(value = "/products", method = RequestMethod.POST)
 	public Product create(@RequestBody Product product) {
 		Coupon coupon = couponClient.getCoupon(product.getCouponCode());
 		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
 		return repo.save(product);
-
 	}
 
 	/**
@@ -41,6 +46,11 @@ public class ProductRestController {
 	public Product sendErrorResponse(Product product) {
 		return product;
 
+	}
+
+	@RequestMapping(value="/prop",method = RequestMethod.GET)
+	public String getProp(){
+		return this.prop;
 	}
 
 }
